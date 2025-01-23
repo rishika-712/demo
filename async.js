4e58e38db42b92b05a5658497c2f2a40e33347d8
rishika-712 <angarishika8@gmail.com>
2025-01-23 13:37:23 +0530

    pushing the files

--- async.js
+++ async.js
@@ -16,23 +16,43 @@
 // let p1 = new Promise((resolve , reject)=>{});
 // console.log(p1);
 
-let p2 = new Promise((resolve , reject)=>{
-    resolve("success");
-})
+//let p2 = new Promise((resolve , reject)=>{
+  //  resolve("success");
+//})
 // console.log(p2);
 // p2
 // .then((response)=>console.log(response))
 // .catch((error)=>console.log(error))
 // .finally(()=>console.log("finally for both"))
 
-let p3 = new Promise((resolve , reject)=>{
-    reject("Failure");
-});
+//let p3 = new Promise((resolve , reject)=>{
+  //  reject("Failure");
+//});
 // console.log(p3);
-p3
-.then(data=>console.log(data))
-.catch(err=>console.log(err))
-.finally(()=>console.log("finally for both"))
+//p3
+//.then(data=>console.log(data))
+//.catch(err=>console.log(err))
+//.finally(()=>console.log("finally for both"))
+let p = new Promise((resolve , reject)=>{
+    setTimeout(()=>{
+        resolve("success");
+    }, 5000)
+})
 
+ async function demo(){
+    console.log("Start");
+    let x = p;
+    console.log(x);
+    console.log("End");
+};
+demo();
+
+async function fetchUsers()
+{
+    let response =await fetch("https://jsonplaceholder.typicode.com/users");
+    let data =await response.json();
+    console.log(data);
+}
+fetchUsers();
 
 
