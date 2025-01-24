aaed17fd5670d95674d868a40fd4d1a47ec6dc8f
rishika-712 <angarishika8@gmail.com>
2025-01-24 13:28:48 +0530

    pushing the files

---
+++ dom.js
@@ -0,0 +1,14 @@
+// let ele = document.getElementsByClassName("test");
+// console.log(ele);
+// console.log(Array.isArray(ele));
+// ele[0].innerText="Hello"
+// //ele[0].computedStyleMap.backgroundcolor="tomato"
+// //!spread operator :- it will takeout each and every value from the original array
+// //! and store it inside and more new array(pure array).
+// let x = [...ele];
+// x.map(element=>{
+//     element.Style.backgroundColor="orange"
+// })
+
+let ele = document.querySelectorAll(".test");
+console.log(ele);
