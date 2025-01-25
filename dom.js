72e6a3c96b691385f8768cdd39eaa0e36f6cc45d
rishika-712 <angarishika8@gmail.com>
2025-01-25 12:57:26 +0530

    pushing the files

--- dom.js
+++ dom.js
@@ -10,5 +10,39 @@
 //     element.Style.backgroundColor="orange"
 // })
 
-let ele = document.querySelectorAll(".test");
-console.log(ele);
+// let ele = document.querySelectorAll(".test");
+// console.log(ele);
+
+// let btn = document.querySelector("#btn");
+//! addEventListener("event ,()=>{}")
+
+// let ele = document.createElement("hi");
+// ele.innerText = "Dynamic creation of html element";
+// ele.id = "demo";
+
+// let image = document.createElement("img");
+// image.src = "./3606208.jpg";
+// console.log(image);
+
+// document.body.appendChild(ele);
+// document.body.appendChild(image);
+
+let form = document.querySelector("form");
+let username = document.getElementById("uName");
+let mail = document.getElementById("uMail");
+let psw = document.getElementById("uPass");
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    let uname = username.value;
+    let uemail = mail.value;
+    let upass = psw.value;
+    let userDetails={
+        uname , uemail, upass
+    }
+    console.log(userDetails);
+    localStorage.setItem("userData" , JSON.stringify(userDetails))
+
+})
+
+
